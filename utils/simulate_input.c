af8d7d47400596b905f48f875d93115fbfa12c8b
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-08-14 20:32:41 +0200

    rec: Protect functions and improve style

--- utils/simulate_input.c
+++ utils/simulate_input.c
@@ -1,4 +1,5 @@
 #include <libgen.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
@@ -6,33 +7,63 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+enum exit_codes
+{
+	SUCCESS = 0,
+	INPUT_ERR,
+	PIPE_ERR,
+	FORK_ERR,
+	IOCTL_ERR,
+	EXEC_ERR
+};
+
+static int	parent(char *argv[], int pipe_fds[]);
+static void	child(char *argv[], int pipe_fds[]);
+
 int	main(int argc, char *argv[])
 {
 	int		pipe_fds[2];
 	pid_t	pid;
 
 	if (argc < 3)
-		return (1);
-
-	pipe(pipe_fds);
-
+		return (INPUT_ERR);
+	if (pipe(pipe_fds) == -1)
+		return (PIPE_ERR);
 	pid = fork();
-	if (pid == 0)
-	{
-		close(pipe_fds[1]);
-		dup2(pipe_fds[0], STDIN_FILENO);
-		close(pipe_fds[0]);
-		execvp(argv[1], (char *[]){basename(argv[1]), NULL});
-		exit(2);
-	}
+	if (pid == -1)
+		return (FORK_ERR);
+	else if (pid == 0)
+		child(argv, pipe_fds);
+	else
+		return (parent(argv, pipe_fds));
+}
+
+static int	parent(char *argv[], int pipe_fds[])
+{
+	bool	error = false;
 
 	close(pipe_fds[0]);
 	for (size_t i = 0; argv[2][i]; i++)
-		ioctl(STDIN_FILENO, TIOCSTI, &argv[2][i]);
-	ioctl(STDIN_FILENO, TIOCSTI, "\n");
+	{
+		if (ioctl(STDIN_FILENO, TIOCSTI, &argv[2][i]) == -1)
+		{
+			error = true;
+			break ;
+		}
+	}
+	if (!error)
+		if (ioctl(STDIN_FILENO, TIOCSTI, "\n") == -1)
+			error = true;
 	close(pipe_fds[1]);
-
 	wait(NULL);
+	return (error ? IOCTL_ERR : SUCCESS);
+}
 
-	return (0);
+static void	child(char *argv[], int pipe_fds[])
+{
+	close(pipe_fds[1]);
+	dup2(pipe_fds[0], STDIN_FILENO);
+	close(pipe_fds[0]);
+	execvp(argv[1], (char *[]){basename(argv[1]), NULL});
+	exit(EXEC_ERR);
 }
