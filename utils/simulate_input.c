bcaac057a047864c755646f39597521d2967bc67
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-08-14 14:05:23 +0200

    rec: Try to simulate real terminal, but doesn't work yet

--- utils/simulate_input.c
+++ utils/simulate_input.c
@@ -1,38 +1,99 @@
+#define _XOPEN_SOURCE 600
+#define _GNU_SOURCE
+
 #include <libgen.h>
-#include <stddef.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <termios.h>
 
-int	main(int argc, char *argv[])
-{
-	int		pipe_fds[2];
-	pid_t	pid;
+int main(int argc, char *argv[]) {
+    int master, slave;
+    pid_t pid;
+    char *slave_name;
 
-	if (argc < 3)
-		return (1);
+    if (argc < 3)
+        return 1;
 
-	pipe(pipe_fds);
+    // Open pseudo-terminal
+    master = posix_openpt(O_RDWR | O_NOCTTY);
+    if (master == -1) {
+        perror("posix_openpt");
+        return 1;
+    }
 
-	pid = fork();
-	if (pid == 0)
-	{
-		close(pipe_fds[1]);
-		dup2(pipe_fds[0], STDIN_FILENO);
-		close(pipe_fds[0]);
-		execvp(argv[1], (char *[]){basename(argv[1]), NULL});
-		exit(2);
+    if (grantpt(master) == -1) {
+        perror("grantpt");
+        return 1;
+    }
+
+    if (unlockpt(master) == -1) {
+        perror("unlockpt");
+        return 1;
+    }
+
+    slave_name = ptsname(master);
+    if (slave_name == NULL) {
+        perror("ptsname");
+        return 1;
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return 1;
+    }
+	// Open the slave side of the PTY
+	slave = open(slave_name, O_RDWR);
+	if (slave == -1) {
+		perror("open slave");
+		exit(1);
 	}
 
-	close(pipe_fds[0]);
-	for (size_t i = 0; argv[2][i]; i++)
-		ioctl(STDIN_FILENO, TIOCSTI, &argv[2][i]);
-	ioctl(STDIN_FILENO, TIOCSTI, "\n");
-	close(pipe_fds[1]);
+    if (pid == 0) {  // Child process
+        close(master);
 
-	wait(NULL);
 
-	return (0);
+        // Create a new session and set the slave PTY as the controlling terminal
+        if (setsid() == -1) {
+            perror("setsid");
+            exit(1);
+        }
+
+        if (ioctl(slave, TIOCSCTTY, 0) == -1) {
+            perror("ioctl TIOCSCTTY");
+            exit(1);
+        }
+
+        // Redirect stdin, stdout, and stderr to the slave PTY
+        dup2(slave, STDIN_FILENO);
+        dup2(slave, STDOUT_FILENO);
+        dup2(slave, STDERR_FILENO);
+
+        close(slave);
+
+        execvp(argv[1], (char *[]){basename(argv[1]), NULL});
+        perror("execvp");
+        exit(2);
+    }
+
+    // Parent process
+    close(slave);
+
+    // Write input to the master side of the PTY
+    write(master, argv[2], strlen(argv[2]));
+    write(master, "\n", 1);
+
+    // Close the master side to signal EOF to the child
+    close(master);
+
+    // Wait for the child to finish
+    wait(NULL);
+
+    return 0;
 }
